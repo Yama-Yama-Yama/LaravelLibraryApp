3a34ba9bcecf5d5e03d7d4ea3415077b78d903e0
Yama <83229568+Yama-Yama-Yama@users.noreply.github.com>
2022-05-05 19:50:52 +0200

    First commit

---
+++ database/migrations/2022_03_30_7_create_book_issues_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_issues', function (Blueprint $table) {
+            $table->id();
+            $table->date('issue_date');
+            $table->date('return_day')->nullable();
+            $table->boolean('issue_status');
+            $table->timestamps();
+            $table->foreignId('customer_id')->constrained();
+            $table->foreignId('book_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_issues');
+    }
+};
